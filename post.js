1223d6ff31a6606da053e810538d2b306cab4b35
olubanwo Ayomide <olubanwoayomide@gmail.com>
2020-07-25 22:38:58 +0100

    new commit

---
+++ post.js
@@ -0,0 +1,20 @@
+const posts = [
+	{title: 'post one' ,  body; 'this is post one'}
+	{title: 'post two' ,  body; 'this is post two'}
+];
+
+function createPost(post){
+	setTimeout(function(){
+		posts.push(post);
+	}, 2000)
+}
+
+function getPost(){
+	setTimeout(function(){
+		let output = '';
+		posts.forEach(function(post){
+			output += `<li>${post.title}</li>`;
+		});
+		document.body.innerHTML = output;
+	}, 2000)
+}
