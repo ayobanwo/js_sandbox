78312ac4fd4977b910849cbd84fbad3d862b729e
olubanwo Ayomide <olubanwoayomide@gmail.com>
2020-07-25 23:12:15 +0100

    new commit

--- post.js
+++ post.js
@@ -1,15 +1,35 @@
 const posts = [
-	{title: 'post one' ,  body; 'this is post one'}
-	{title: 'post two' ,  body; 'this is post two'}
+	{title: 'Post One' ,  body: 'this is post one'},
+	{title: 'Post Two' ,  body: 'this is post two'}
 ];
 
-function createPost(post){
-	setTimeout(function(){
+// function createPost(post){
+// 	setTimeout(function(){ 
+// 		posts.push(post);
+// 	}, 2000)
+// }
+
+// function getPosts(){
+// 	setTimeout(function(){
+// 		let output = '';
+// 		posts.forEach(function(post){
+// 			output += `<li>${post.title}</li>`;
+// 		});
+// 		document.body.innerHTML = output;
+// 	}, 2000)
+// }
+
+// createPost({title: 'Post Three' ,  body; 'this is post Three'});
+// getPosts();
+
+function createPost(post, callback){
+	setTimeout(function(){ 
 		posts.push(post);
-	}, 2000)
+		callback();
+	}, 2000);
 }
 
-function getPost(){
+function getPosts(){
 	setTimeout(function(){
 		let output = '';
 		posts.forEach(function(post){
@@ -18,3 +38,5 @@ function getPost(){
 		document.body.innerHTML = output;
 	}, 2000)
 }
+
+createPost({title: 'Post Three' ,  body: 'this is post Three'}, getPosts);
