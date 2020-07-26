8eba6a9b6571692293bf638668ef5314c948eb89
olubanwo Ayomide <olubanwoayomide@gmail.com>
2020-07-27 00:36:53 +0100

    post request

--- easy.js
+++ easy.js
@@ -1,12 +1,40 @@
 const http = new easyHTTP;
 
-//Get Posts
-http.get('http://jsonplaceholder.typicode.com/posts', 
-	function(err, posts){
-		if(err){
-			console.log(err)
-		}
-		else{
-			console.log(posts);
-		}
-	});
+// //Get single Post
+// http.get('https://jsonplaceholder.typicode.com/posts/1', 
+// 	function(err, post){
+// 		if(err){
+// 			console.log(err)
+// 		}
+// 		else{
+// 			console.log(post);
+// 		}
+// 	});
+
+// //Get Posts
+// http.get('http://jsonplaceholder.typicode.com/posts', 
+// 	function(err, posts){
+// 		if(err){
+// 			console.log(err)
+// 		}
+// 		else{
+// 			console.log(posts);
+// 		} 
+// 	});
+
+//Create Data
+const data = {
+	title: 'Custom Post',
+	body: 'This is a custom post'
+};
+
+//Create Post
+http.post('http://jsonplaceholder.typicode.com/posts', data, 
+function(err, post){
+	if(err){
+		console.log(err)
+	}
+	else{
+		console.log(post);
+	}
+})
