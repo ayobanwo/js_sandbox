63e9e9c664aaf0268998f558aea8dcfa54321337
olubanwo Ayomide <olubanwoayomide@gmail.com>
2020-07-26 16:59:04 +0100

    async js using callback

--- easy.js
+++ easy.js
@@ -1,4 +1,7 @@
 const http = new easyHTTP;
 
 //Get Posts
-http.get('http://jsonplaceholder.typicode.com/posts');
+http.get('http://jsonplaceholder.typicode.com/posts', 
+	function(posts){
+		console.log(posts);
+	});
