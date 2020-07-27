8aa6b1282d942cb91324e238f1e2ee9137626df2
olubanwo Ayomide <olubanwoayomide@gmail.com>
2020-07-27 01:06:44 +0100

    delete request

--- easy.js
+++ easy.js
@@ -39,13 +39,23 @@ const data = {
 // 	}
 // })
 
-Update Post
-http.put('https://jsonplaceholder.typicode.com/posts/1', data, function(err, post){
+//Update Post
+// http.put('https://jsonplaceholder.typicode.com/posts/1', data, function(err, post){
+// 	if(err){
+// 		console.log(err)
+// 	}
+// 	else{
+// 		console.log(post);
+// 	}
+// })
+
+//Delete Post
+http.delete('http://jsonplaceholder.typicode.com/posts/1', 
+function(err, response){
 	if(err){
 		console.log(err)
 	}
 	else{
-		console.log(post);
-	}
-})
-
+		console.log(response);
+	} 
+});
