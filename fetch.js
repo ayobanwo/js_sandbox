5a842d4f84614cdbfda2180b78bf2ea22318d2e5
olubanwo Ayomide <olubanwoayomide@gmail.com>
2020-07-30 22:43:18 +0100

    Get JSON data

--- fetch.js
+++ fetch.js
@@ -1,5 +1,8 @@
 document.getElementById('button1').addEventListener('click', getText);
 
+document.getElementById('button2').addEventListener('click', getJson);
+
+//Get Local Text File Data
 function getText(){
 	fetch('test.txt')
 		.then(function(res){
@@ -8,6 +11,25 @@ function getText(){
 		.then(function(data){
 			console.log(data);
 			document.getElementById('output').innerHTML = data ;
+		})
+		.catch(function(err){
+			console.log(err);
+		});
+}  
+
+//Get Local Json data
+function getJson(){
+	fetch('fetch.json')
+		.then(function(res){
+			return res.json();
+		})
+		.then(function(data){
+			console.log(data);
+			let output = '';
+			data.forEach(function(post) {
+				output += `<li>${post.body}</li>`;
+			});
+			document.getElementById('output').innerHTML = output ;
 		})
 		.catch(function(err){
 			console.log(err);
