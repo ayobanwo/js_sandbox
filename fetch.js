f7dcc2ddc77acdc03ac2b832ca06cacd5b6bcde6
olubanwo Ayomide <olubanwoayomide@gmail.com>
2020-07-30 22:55:47 +0100

    Get External API Data

--- fetch.js
+++ fetch.js
@@ -2,6 +2,8 @@ document.getElementById('button1').addEventListener('click', getText);
 
 document.getElementById('button2').addEventListener('click', getJson);
 
+document.getElementById('button3').addEventListener('click', getExternal);
+
 //Get Local Text File Data
 function getText(){
 	fetch('test.txt')
@@ -28,6 +30,25 @@ function getJson(){
 			let output = '';
 			data.forEach(function(post) {
 				output += `<li>${post.body}</li>`;
+			});
+			document.getElementById('output').innerHTML = output ;
+		})
+		.catch(function(err){
+			console.log(err);
+		});
+}  
+
+//Get API data
+function getExternal(){
+	fetch('https://api.github.com/users')
+		.then(function(res){
+			return res.json();
+		})
+		.then(function(data){
+			console.log(data);
+			let output = '';
+			data.forEach(function(user) {
+				output += `<li>${user.login}</li>`;
 			});
 			document.getElementById('output').innerHTML = output ;
 		})
