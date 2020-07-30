80c080a2dd40b0eba87b8385d083c46e9e568666
olubanwo Ayomide <olubanwoayomide@gmail.com>
2020-07-30 23:47:50 +0100

    basic use of arrow functions

---
+++ arrow.js
@@ -0,0 +1,17 @@
+//Normal funciton 
+// const sayHello = function(){
+// 	console.log('hello');
+// }
+
+//Arrow Function
+// const sayHello = () => {
+// 	console.log('Hello');
+// }
+
+//Single Line Arrow Function
+// const sayHello = () => console.log('Hello');
+
+//Return Object
+const sayHello = () =>( {msg:hello} )
+
+console.log(sayHello());
